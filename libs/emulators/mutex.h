68be01d677786da06c1c6150417b23df2f40401d
DavidC001 <davidecavicchini@hotmail.com>
2022-12-08 00:26:13 +0100

    aggiunti i tipi uintX_t

--- libs/emulators/mutex.h
+++ libs/emulators/mutex.h
@@ -7,6 +7,10 @@
 typedef unsigned char uint_fast8_t;
 typedef unsigned short uint_fast16_t;
 
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void initMutex()
