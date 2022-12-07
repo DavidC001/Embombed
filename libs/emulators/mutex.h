6540ea4e32a3d429f3a7e8270877e0f4802f84e9
DavidC001 <davidecavicchini@hotmail.com>
2022-12-08 00:10:51 +0100

    script supporto per stampa sincrona

---
+++ libs/emulators/mutex.h
@@ -0,0 +1,22 @@
+#ifndef __MUTEX_H__
+#define __MUTEX_H__
+
+//lib to get IRQs to work in mutex
+#include <pthread.h>
+
+typedef unsigned char uint_fast8_t;
+typedef unsigned short uint_fast16_t;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void initMutex()
+{
+    pthread_mutexattr_t mymutexattr;
+    pthread_attr_t myattr; 
+
+    pthread_mutexattr_init(&mymutexattr);
+    pthread_mutex_init(&mutex, &mymutexattr);
+    pthread_mutexattr_destroy(&mymutexattr);
+}
+
+#endif
