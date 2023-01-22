63616532a6c63bf6a9c208e5ca7d0555a6d94db6
DavidC001 <davidecavicchini@hotmail.com>
2023-01-22 23:29:52 +0100

    server python per vedere il manuale

---
+++ python/server/server.py
@@ -0,0 +1,24 @@
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        #send the manual
+        with open("C:\\Users\\david\\Dropbox\\embed\\project\\git\\Embombed\\python\\genGame\\manual\\manual.html", 'r') as infile:
+            self.wfile.write(infile.read().encode())
+        return
+
+def run(server_class=HTTPServer, handler_class=Handler):
+    try:
+        print("Server started")
+        server_address = ('', 8080)
+        httpd = server_class(server_address, handler_class)
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+        print("Server stopped")
+    
+run()
