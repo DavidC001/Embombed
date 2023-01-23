bef1c5fd85d2091fc4865a740e0b74265fda2d5e
DavidC001 <davidecavicchini@hotmail.com>
2023-01-23 15:54:25 +0100

    da thread a process python script

--- python/game/main.py
+++ python/game/main.py
@@ -2,18 +2,26 @@ import server
 import generate
 import json
 from seriale import sendMessagge, awaitResponse, close
-import threading
+import multiprocessing
 
-serverThread = threading.Thread(target=server.run)
-serverThread.start()
+def processJobs():
+    #write to manual.html message to start press reset button on MSP432
+    with open("manual.html", 'w') as outfile:
+        outfile.write("<html><head><title>Manual</title></head><body><h1>Manual</h1><p>Press the reset button on the MSP432</p></body></html>")
 
-while(1):
-    awaitResponse(2)
-    gameJson = generate.generateGame()
-    generate.genManual()
-    #stringify json
-    msg = json.dumps(gameJson)
-    sendMessagge(msg, 2)
+    serverProcess = multiprocessing.Process(target=server.run)
+    serverProcess.start()
 
-close()
-serverThread.join()
+    while(1):
+        awaitResponse(2)
+        gameJson = generate.generateGame()
+        generate.genManual()
+        #stringify json
+        msg = json.dumps(gameJson)
+        sendMessagge(msg, 2)
+
+    close()
+    serverProcess.terminate()
+
+if __name__ == '__main__':
+    processJobs()
