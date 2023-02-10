a71bfdca7c8f3576cc37516e0119b75760452bb9
DavidC001 <davidecavicchini@hotmail.com>
2023-02-10 18:20:29 +0100

    aggiornati commenti per PWM

--- libs/buzzer.h
+++ libs/buzzer.h
@@ -8,17 +8,17 @@
 
 /* Timer_A Compare Configuration Parameter  (PWM) */
 Timer_A_CompareModeConfig compareConfig_PWM = {
-TIMER_A_CAPTURECOMPARE_REGISTER_4,          // Use CCR3
+TIMER_A_CAPTURECOMPARE_REGISTER_4,                  // Use CCR3
         TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE,   // Disable CCR interrupt
         TIMER_A_OUTPUTMODE_TOGGLE_SET,              // Toggle output but
-        0                                    
+        0                                           // compareValue       
         };
 
 /* Timer_A Up Configuration Parameter */
 Timer_A_UpModeConfig upConfig = {
-        TIMER_A_CLOCKSOURCE_SMCLK,               // SMCLK = 48 MhZ
+        TIMER_A_CLOCKSOURCE_SMCLK,              // SMCLK = 48 MhZ
         TIMER_A_CLOCKSOURCE_DIVIDER_12,         // SMCLK/12 = 4 MhZ
-        4000,                                   
+        4000,                                   //timerPeriod                         
         TIMER_A_TAIE_INTERRUPT_DISABLE,         // Disable Timer interrupt
         TIMER_A_CCIE_CCR0_INTERRUPT_DISABLE,    // Disable CCR0 interrupt
         TIMER_A_DO_CLEAR                        // Clear value
